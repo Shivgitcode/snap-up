8c4ccad13d23a04701dfe11a15d21a7f186d399e
shivgitcode <shivneeraj2004@gmail.com>
2024-09-30 01:03:34 +0530

    almost done

--- src/routers/router.ts
+++ src/routers/router.ts
@@ -1,8 +1,18 @@
 import { t } from "@/server/server";
+import { z } from "zod";
+import axios from "axios"
 
 export const appRouter = t.router({
-    getStatus: t.procedure.query(() => {
-        return { webstatus: "up" }
+    getStatus: t.procedure.input(z.object({ url: z.string().url(), interval: z.number(), name: z.string() })).query(async ({ input }) => {
+        const { url, interval, name } = input
+        const response = await axios.get(url);
+        if (response.status == 200) {
+            return { message: "up", isUp: true }
+        }
+        else {
+            return { message: "down", isUp: false }
+        }
+
     })
 })
 
