bd5fc2be6a4025a146b5b6ee9ca3e6d75f39a158
shivneeraj2004@gmail.com <shivneeraj2004@gmail.com>
2024-09-29 23:53:16 +0530

    doing dashboard again

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,19 +5,19 @@ import { Lock } from "lucide-react";
 export default function About() {
     return (
         <div className="w-full py-[100px]">
-            <div className="w-[60%] mx-auto flex flex-col items-center gap-8">
-                <div className="flex flex-col items-center gap-5">
+            <div className="w-[70%] mx-auto flex flex-col items-center">
+                <div className="flex flex-col items-center gap-8 mb-12">
                     <h2 className=" text-mainbg text-[36px] font-bold leading-[40px]">What Uptime Monitor can do?</h2>
                     <p className=" text-[#6b7280] text-[20px] leading-[28px] w-[60%] text-center">Uptime Monitor is a powerful tool for ensuring the availability of your websites and servers. With our service, you can easily keep track of your online assets and receive alerts when there are any issues or disruptions.</p>
                 </div>
-                <div className="grid grid-cols-3 grid-rows-2 gap-8 w-full">
+                <div className="flex flex-row justify-center flex-wrap w-full gap-8">
                     {about.map(el => (
-                        <div className="flex flex-col items-center shadow-md  bg-[#f1f5f9] rounded-md min-h-[256px] px-[24px] py-[32px] relative">
+                        <div className="flex flex-col items-center shadow-md  bg-[#f1f5f9] rounded-md  max-w-[300px] px-[24px] py-[32px] relative">
                             <div className="bg-gradient-to-r from-teal-500 to-cyan-600 rounded-md p-2 absolute -top-5">
                                 <Lock />
                             </div>
                             <div className="flex flex-col items-center gap-3">
-                                <h3 className="font-bold ">{el.head}</h3>
+                                <h3 className="font-bold text-center ">{el.head}</h3>
                                 <p className="text-center text-[#6b7280]">{el.desc}</p>
                             </div>
                         </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,30 @@
 import { Contact, Globe, LogOut, Monitor } from "lucide-react"
 import Link from "next/link"
+import { Button } from "./ui/button"
 
 export default function Sidebar() {
-    const side=[
-        {id:1,name:"Monitors",icon:Monitor},
-        {id:2,name:"Contacts",icon:Contact},
-        {id:3,name:"Status Page",icon:Globe},
+    const side = [
+        { id: 1, name: "Monitors", icon: Monitor },
+        { id: 2, name: "Contacts", icon: Contact },
+        { id: 3, name: "Status Page", icon: Globe },
     ]
-  return (
-    <div className=" bg-[#334155] py-[32px] px-[16px] min-w-[300px]">
-        <nav className="flex flex-col justify-between min-h-full">
-            <div>
-                <h1 className="text-white font-bold text-[30px] mb-[26px]">Snap Up</h1>
-                <ul>
-                    {side.map(el=>{
-                        return <li className="flex items-center gap-5 mb-10 text-white"><el.icon/><span>{el.name}</span></li>
-                    })}
-                </ul>
+    return (
+        <div className=" bg-[#334155] py-[32px] px-[16px] min-w-[300px]">
+            <nav className="flex flex-col justify-between min-h-full">
+                <div>
+                    <h1 className="text-white font-bold text-[30px] mb-[26px]">Snap Up</h1>
+                    <ul>
+                        {side.map(el => {
+                            return <li><Button variant={"ghost"} className="flex items-center gap-5 mb-10 text-white hover:bg-transparent hover:text-white"><el.icon /><span>{el.name}</span></Button></li>
+                        })}
+                    </ul>
 
-            </div>
-            <div className="flex items-center text-white gap-5">
-                <LogOut></LogOut>
-                <button>logout</button>
-            </div>
-        </nav>
-    </div>
-  )
+                </div>
+                <div className="flex items-center text-white gap-5">
+                    <LogOut></LogOut>
+                    <button>logout</button>
+                </div>
+            </nav>
+        </div>
+    )
 }
